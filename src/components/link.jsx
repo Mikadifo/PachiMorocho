d0f514d190b1efa75148264155eef9f254637d6c
Mikadifo <mikadifopadillaheredia@gmail.com>
2025-06-18 20:02:36 -0400

    add: hover on links

--- src/components/link.jsx
+++ src/components/link.jsx
@@ -2,9 +2,13 @@ import linkIcon from "./../assets/linkIcon.svg";
 
 export default function Link({ label }) {
   return (
-    <button className="flex gap-1 text-dark font-heading font-bold text-xl cursor-pointer">
-      <img src={linkIcon} alt="Link Icon" />
-      <span>{label}</span>
+    <button className="flex gap-1 text-dark font-heading font-bold text-xl cursor-pointer group">
+      <img
+        src={linkIcon}
+        alt="Link Icon"
+        className="transition-transform duration-300 group-hover:rotate-12"
+      />
+      <span className="group-hover:opacity-80">{label}</span>
     </button>
   );
 }
